2239409c1179f3c6efc1b323cdc7aca44a9706bb
nikitazapekin <wotblitz362@mail.ru>
2024-07-25 03:44:35 +0300

    fix issues

--- scripts/sidebar.js
+++
@@ -1,7 +0,0 @@
-const burgerBtn = document.querySelector(".burger__btn")
-const burgerContent = document.querySelector(".burger__content")
-let isOpenBurger = false
-burgerBtn.addEventListener("click", ()=> {
-    console.log(1)
-burgerContent.style.transform = "translateX(0%)"
-})
